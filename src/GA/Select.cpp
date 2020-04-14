3bbea77e1e5b6cc5bb0ee1cf0bb71266b47f145f
cxy004 <cxy004@qq.com>
2020-04-14 23:14:52 +0800

    select

--- src/GA/Select.cpp
+++ src/GA/Select.cpp
@@ -10,7 +10,7 @@ void Select(std::vector<Individual> &dst, std::size_t num) {
 	for(std::size_t i = 0; i < dst.size(); ++i)
 		pr[i] = dst[i].fitness;
 	std::discrete_distribution<> choice(pr.begin(), pr.end());
-	res.resize(dst.size());
+	res.resize(num);
 	for(std::size_t i = 0; i < num; ++i)
 		res[i] = dst[choice(rand_engine)];
 	dst = std::move(res);
