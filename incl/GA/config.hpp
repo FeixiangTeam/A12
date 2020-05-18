0078494aa4d7cd0811b5a608a71fef7035e73feb
lemonaaaaa <2275689493@qq.com>
2020-05-18 12:52:55 +0800

    config

--- incl/GA/config.hpp
+++ incl/GA/config.hpp
@@ -3,10 +3,10 @@
 
 constexpr int POPULATION = 100;
 constexpr int TIMES = 100;
-constexpr int MUTATION_TRY_TIMES = 5;
-constexpr double PR_CROSS = 0.6;
-constexpr double PR_MUTATION = 0.05;
-constexpr double BETA = 0.05;
+constexpr int MUTATION_TRY_TIMES = 25;
+constexpr double PR_CROSS = 0.55;
+constexpr double PR_MUTATION = 0.28;
+constexpr double BETA = 0.5;
 constexpr double FITNESS_FACTOR = 100;
 
 #endif
